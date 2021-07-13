4921a96557781324e25f62aecc8c14b3149a2ad1
desmodreamer <aqeelbhat@icloud.com>
2021-07-13 17:35:54 +0530

    Add custom corner radii access modifier
    This allows to control corner radii of a specific corner to a quadrilateral

--- NewsOrgApp/NewsOrgApp/Extensions/View+Extensions.swift
+++ NewsOrgApp/NewsOrgApp/Extensions/View+Extensions.swift
@@ -8,4 +8,9 @@ extension View {
     }
 }
 
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
+}
 
